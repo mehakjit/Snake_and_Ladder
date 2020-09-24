4e78015372388c1b0038f061d25464a46c20983c
mehakjit <mehakjitsingh@gmail.com>
2020-09-24 14:26:46 +0530

    [Mehakjit]Created: Die Roll

--- src/capgemini/Snake_and_Ladder.java
+++ src/capgemini/Snake_and_Ladder.java
@@ -6,7 +6,10 @@ public class Snake_and_Ladder {
 	System.out.println("Welcome to Snake and Ladder");
 	int No_of_Players = 1;
 	int position = 0;
+	int die_roll = 0;
 	for (int i = 1;i < No_of_Players + 1;i++ )
 		System.out.println("The position of Player " + i + " is " + position );
+		die_roll = (int)(Math.random()*6+1);
+		System.out.println("The die rolls for player 1 is " + die_roll);
 	}
 }
